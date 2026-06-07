4bb142ed7f1da16cc84c0163977b69c28c8854d0
vennavallividyadhari-spec <vennavallividyadhari@gmail.com>
2026-06-07 12:34:56 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+print("Simple Calculator")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator")
